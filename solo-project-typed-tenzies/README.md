b723e3c5fdc1f8cb262a53cf388da5ebdfad3de2
jubayer.webdev <jubayer.webdev@gmail.com>
2026-04-23 08:04:13 +0600

    typed-tenzies: set up the tenzies project

--- solo-project-typed-tenzies/README.md
+++ solo-project-typed-tenzies/README.md
@@ -1,73 +1,27 @@
-# React + TypeScript + Vite
+## 📦 Packages
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- [nanoid](https://www.npmjs.com/package/nanoid)
+- [react-confetti](https://www.npmjs.com/package/react-confetti)
 
-Currently, two official plugins are available:
+---
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
+## 🚀 How to run
 
-## React Compiler
+Please follow the below instructions to run this branch in your machine:
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules:
-
-```js
-export default defineConfig([
-    globalIgnores(["dist"]),
-    {
-        files: ["**/*.{ts,tsx}"],
-        extends: [
-            // Other configs...
-
-            // Remove tseslint.configs.recommended and replace with this
-            tseslint.configs.recommendedTypeChecked,
-            // Alternatively, use this for stricter rules
-            tseslint.configs.strictTypeChecked,
-            // Optionally, add this for stylistic rules
-            tseslint.configs.stylisticTypeChecked,
-
-            // Other configs...
-        ],
-        languageOptions: {
-            parserOptions: {
-                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-                tsconfigRootDir: import.meta.dirname,
-            },
-            // other options...
-        },
-    },
-]);
-```
-
-You can also install [eslint-plugin-react-x](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-x) and [eslint-plugin-react-dom](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-dom) for React-specific lint rules:
-
-```js
-// eslint.config.js
-import reactX from "eslint-plugin-react-x";
-import reactDom from "eslint-plugin-react-dom";
-
-export default defineConfig([
-    globalIgnores(["dist"]),
-    {
-        files: ["**/*.{ts,tsx}"],
-        extends: [
-            // Other configs...
-            // Enable lint rules for React
-            reactX.configs["recommended-typescript"],
-            // Enable lint rules for React DOM
-            reactDom.configs.recommended,
-        ],
-        languageOptions: {
-            parserOptions: {
-                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-                tsconfigRootDir: import.meta.dirname,
-            },
-            // other options...
-        },
-    },
-]);
-```
+1. Clone this repository -
+    ```bash
+    git clone https://github.com/jubayer-webdev/scrimba-learn-typescript.git
+    ```
+2. Navigate to the project directory
+    ```bash
+    cd scrimba-learn-typescript/solo-project-typed-tenzies
+    ```
+3. Install node dependencies
+    ```bash
+    npm i
+    ```
+4. Start the development server
+    ```bash
+    npm run dev
+    ```
--- solo-project-typed-tenzies/src/App.tsx
+++ solo-project-typed-tenzies/src/App.tsx
@@ -1,169 +1,72 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import { useState, useRef, useEffect } from "react";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
-function App() {
-    const [count, setCount] = useState(0);
+export default function App() {
+    const [dice, setDice] = useState(() => generateAllNewDice());
+    const buttonRef = useRef(null);
+
+    const gameWon = dice.every((die) => die.isHeld) && dice.every((die) => die.value === dice[0].value);
+
+    useEffect(() => {
+        if (gameWon) {
+            buttonRef.current.focus();
+        }
+    }, [gameWon]);
+
+    function generateAllNewDice() {
+        return new Array(10).fill(0).map(() => ({
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid(),
+        }));
+    }
+
+    function rollDice() {
+        if (!gameWon) {
+            setDice((oldDice) =>
+                oldDice.map((die) => (die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
+            );
+        } else {
+            setDice(generateAllNewDice());
+        }
+    }
+
+    function hold(id) {
+        setDice((oldDice) => oldDice.map((die) => (die.id === id ? { ...die, isHeld: !die.isHeld } : die)));
+    }
+
+    const diceElements = dice.map((dieObj) => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ));
 
     return (
-        <>
-            <section id="center">
-                <div className="hero">
-                    <img
-                        src={heroImg}
-                        className="base"
-                        width="170"
-                        height="179"
-                        alt=""
-                    />
-                    <img
-                        src={reactLogo}
-                        className="framework"
-                        alt="React logo"
-                    />
-                    <img
-                        src={viteLogo}
-                        className="vite"
-                        alt="Vite logo"
-                    />
-                </div>
-                <div>
-                    <h1>Get started</h1>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-                    </p>
-                </div>
-                <button
-                    className="counter"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    Count is {count}
-                </button>
-            </section>
-
-            <div className="ticks"></div>
-
-            <section id="next-steps">
-                <div id="docs">
-                    <svg
-                        className="icon"
-                        role="presentation"
-                        aria-hidden="true"
-                    >
-                        <use href="/icons.svg#documentation-icon"></use>
-                    </svg>
-                    <h2>Documentation</h2>
-                    <p>Your questions, answered</p>
-                    <ul>
-                        <li>
-                            <a
-                                href="https://vite.dev/"
-                                target="_blank"
-                            >
-                                <img
-                                    className="logo"
-                                    src={viteLogo}
-                                    alt=""
-                                />
-                                Explore Vite
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://react.dev/"
-                                target="_blank"
-                            >
-                                <img
-                                    className="button-icon"
-                                    src={reactLogo}
-                                    alt=""
-                                />
-                                Learn more
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div id="social">
-                    <svg
-                        className="icon"
-                        role="presentation"
-                        aria-hidden="true"
-                    >
-                        <use href="/icons.svg#social-icon"></use>
-                    </svg>
-                    <h2>Connect with us</h2>
-                    <p>Join the Vite community</p>
-                    <ul>
-                        <li>
-                            <a
-                                href="https://github.com/vitejs/vite"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#github-icon"></use>
-                                </svg>
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://chat.vite.dev/"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#discord-icon"></use>
-                                </svg>
-                                Discord
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://x.com/vite_js"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#x-icon"></use>
-                                </svg>
-                                X.com
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://bsky.app/profile/vite.dev"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#bluesky-icon"></use>
-                                </svg>
-                                Bluesky
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <div className="ticks"></div>
-            <section id="spacer"></section>
-        </>
+        <main>
+            {gameWon && <Confetti />}
+            <div
+                aria-live="polite"
+                className="sr-only"
+            >
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">
+                Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            </p>
+            <div className="dice-container">{diceElements}</div>
+            <button
+                ref={buttonRef}
+                className="roll-dice"
+                onClick={rollDice}
+            >
+                {gameWon ? "New Game" : "Roll"}
+            </button>
+        </main>
     );
 }
-
-export default App;
